8b56b8ff1bb7e0306918780c23cbbd4a4ecfac9e
shivam mahto <fastranking08@gmail.com>
2025-03-12 17:21:44 +0530

    Updated AvailableSlotDTO and added WebConfig for CORS origin handling

---
+++ appointment-booking/src/main/java/com/fastranking/appointment_booking/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.fastranking.appointment_booking.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Allow CORS for all endpoints under /api/*
+                .allowedOrigins("http://127.0.0.1:8000") // Allow frontend to access the backend
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials (cookies, authentication)
+    }
+}
--- appointment-booking/src/main/java/com/fastranking/appointment_booking/dto/AvailableSlotDTO.java
+++ appointment-booking/src/main/java/com/fastranking/appointment_booking/dto/AvailableSlotDTO.java
@@ -1,5 +1,6 @@
 package com.fastranking.appointment_booking.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,10 @@ import lombok.NoArgsConstructor;
     @AllArgsConstructor
     public class AvailableSlotDTO {
         private Long id;
+        @JsonProperty("start_time")
         private String startTime;
+        @JsonProperty("end_time")
         private String endTime;
+        @JsonProperty("available_capacity")
         private int availableCapacity;
     }
